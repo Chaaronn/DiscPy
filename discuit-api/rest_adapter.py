0091a9c85d1a0ba1ca7cca2ccc25f69c4e131acc
Hadeess <39347872+Chaaronn@users.noreply.github.com>
2023-08-15 11:10:52 +0100

    refactored methods

--- discuit-api/rest_adapter.py
+++ discuit-api/rest_adapter.py
@@ -12,20 +12,28 @@ class RestAdapter:
         if not ssl_verify:
             requests.packages.urllib3.disable_warnings()
 
-    def get(self, endpoint: str, ep_params: Dict = None) -> List[Dict]:
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None):
         full_url = self.url + endpoint
+        # will be required for auth down the line
         headers = {}
-        response = requests.get(full_url, verify=self._ssl_verify, 
-                                headers=headers, params=ep_params)
-        data = response.json()
 
+        response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, 
+                                    headers=headers, params=ep_params, json=data)
+
+        data = response.json()
         if response.status_code >= 200 and response.status_code <= 299: # okay 
             return data
         raise Exception(data['message'])    # todo, raise custom expection
+
+
+    def get(self, endpoint: str, ep_params: Dict = None) -> List[Dict]:
+        return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
     
-    # needs auth to work so adding for future
+    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+        return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
     
-    
+    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+        return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data)
 
 discuitapi = RestAdapter(hostname= "discuit.net/api")
 test_params = {'communityId' : '17692e122def73f25bd757e0'}
