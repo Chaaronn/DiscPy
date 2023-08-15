e5c99946654e64e628a0017dcffed91e9d1112f3
Hadeess <39347872+Chaaronn@users.noreply.github.com>
2023-08-15 11:23:57 +0100

    a

--- discuit-api/rest_adapter.py
+++ discuit-api/rest_adapter.py
@@ -1,6 +1,9 @@
 import requests
 import requests.packages
 from typing import List, Dict
+from exceptions import DiscuitAPIException
+from json import JSONDecodeError
+from models import Result
 
 class RestAdapter:
     def __init__(self, hostname: str, api_key: str = '', ssl_verify: bool = True):
@@ -12,30 +15,37 @@ class RestAdapter:
         if not ssl_verify:
             requests.packages.urllib3.disable_warnings()
 
-    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None):
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
         full_url = self.url + endpoint
         # will be required for auth down the line
         headers = {}
+        try:
+            response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, 
+                                        headers=headers, params=ep_params, json=data)
+        except requests.exceptions.RequestException as e:
+            raise DiscuitAPIException("Request Failed") from e
 
-        response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, 
-                                    headers=headers, params=ep_params, json=data)
+        try:
+            data = response.json()
+        except (ValueError, JSONDecodeError) as e:
+            raise DiscuitAPIException("Bad JSON response") from e
 
-        data = response.json()
-        if response.status_code >= 200 and response.status_code <= 299: # okay 
-            return data
-        raise Exception(data['message'])    # todo, raise custom expection
+        if 299 >= response.status_code >= 200: # okay 
+            # return a result object
+            return Result(response.status_code, message=response.reason, data=data)
+        raise DiscuitAPIException(f"{response.status_code} :  {response.reason}")
 
 
-    def get(self, endpoint: str, ep_params: Dict = None) -> List[Dict]:
+    def get(self, endpoint: str, ep_params: Dict = None) -> Result:
         return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
     
-    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
         return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
     
-    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
         return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data)
 
 discuitapi = RestAdapter(hostname= "discuit.net/api")
 test_params = {'communityId' : '17692e122def73f25bd757e0'}
-post_list = discuitapi.get("posts", ep_params=test_params)
-print(post_list)
+result = discuitapi.get("posts", ep_params=test_params)
+print(result.data)
