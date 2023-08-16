7734bcaf206574d10376966e89ebe62514e8b790
Hadeess <39347872+Chaaronn@users.noreply.github.com>
2023-08-16 09:34:22 +0100

    added more models, added start of more API methods

--- discuit-api/disc_api.py
+++ discuit-api/disc_api.py
@@ -10,29 +10,95 @@ class DiscuitAPI:
         self._rest_adapter = RestAdapter(hostname, api_key, ssl_verify, logger)
     
     def get_all_posts(self) -> Posts:
+        """Gets most recent posts, site-wide.
+
+        Returns:
+            Posts: A list of Post objects.
+        """
         result = self._rest_adapter.get(endpoint='posts')
         posts = Posts(**result.data)
         return posts
     
-    def get_community_posts(self, community_id: str = '') -> Posts:
+    def get_community_posts(self, community_id: str) -> Posts:
+        """Gets most recent posts by Community ID
+
+        Args:
+            community_id (str): The ID of the community to get posts from.
+
+        Returns:
+            Posts: A list of Post objects.
+        """
         result = self._rest_adapter.get(endpoint=f'posts?communityID={community_id}')
         posts = Posts(**result.data)
         return posts
     
     def get_post_by_id(self, post_id: str) -> Posts:
+        """Get a Post object by Public ID
+        Currently returns list of posts, even though its one.
+
+        Args:
+            post_id (str): The Public ID of the post (discuit.com/postId)
+
+        Returns:
+            Posts: List of Post objects.
+        """
         # post ID should be the public ID
         # currently broken, as models are weird
         result = self._rest_adapter.get(endpoint=f'posts/{post_id}')
         post = Posts(**result.data)
         return post[0]
+    
+    def get_post_comments(self, post_id:str) -> Comments: 
+        """Get comments on a post by Public ID
+
+        Args:
+            post_id (str): The Public ID of the post.
+
+        Returns:
+            Comments: A list of Comment objects
+        """
+        result = self._rest_adapter.get(endpoint=f'posts/{post_id}/comments')
+        comments = Comments(**result.data)
+        return comments
 
     def fetch_link_data(self, link: Link):
         link.data = self._rest_adapter.fetch_data(url=link.url)
+
+    def get_communites(self) -> List[Community]:
+        """Gets all communities, sitewide
+
+        Returns:
+            List[Community]: A list of community objects
+        """
+        results = self._rest_adapter.get(endpoint='communities')
+        # this doesnt work for adding to list, result not iterable
+        # cahnged, now missing positional args for Community class??
+        communities = []
+        for result in results.data:
+            communities.append(Community(**result))
+        return communities
+
+    def get_community_by_id(self, community_id: str) -> Community:
+        result = self._rest_adapter.get(endpoint=f'communities/{community_id}')
+        community = Community(**result.data)
+        return community
+
+    def get_community_rules(self, community_id: str):
+        pass
+
+    def get_community_mods(self, community_id: str):
+        pass
+
+    def get_user_by_id(self, user_id: str):
+        pass
+
+    def get_user_by_username(self, username:str):
+        pass
     
 
 api = DiscuitAPI()
 
 #res = api.get_community_posts("177a1ae4ee883ca82b22d914")
-res = api.get_post_by_id("6yiiXeJt")
+res = api.get_community_by_id('177a1ae4ee883ca82b22d914')
 
-print(res)
+print(res) 
--- discuit-api/models.py
+++ discuit-api/models.py
@@ -24,6 +24,9 @@ class Link:
         self.__dict__.update(kwargs)
 
     def save_to(self, path:str = './', file_name: str = ''):
+        '''
+        This should save the link contents to file. 
+        '''
         if not self.data:
             raise DiscuitAPIException("No data to save")
         try:
@@ -50,7 +53,7 @@ class Post:
         self.user_id = user_id                      # ID of user who created post
         self.username = username                    # username of user who submitted
         self.user_group = user_group                # admin, mod, or normal
-        self.user_deleted = user_deleted            # ??? 
+        self.user_deleted = user_deleted            # if the users account is deleted
         self.is_pinned = is_pinned                  # if the post is pinned
         self.community_id = community_id            # ID of the community the post is in
         self.community_name = community_name        # Name of community post is in
@@ -62,12 +65,12 @@ class Post:
         self.locked_at = locked_at                  # Time/Date when it was locked
         self.upvotes = upvotes                      # Number of upvotes
         self.downvotes = downvotes                  # Number of downvotes
-        self.hotness = hotness                      # ???
+        self.hotness = hotness                      # Used to order by 'hot'
         self.created_at = created_at                # When the post was created
         self.edited_at = edited_at                  # If it was edited, when
         self.last_activity_at = last_activity_at    # Time of last post activity 
         self.deleted = deleted                      # If post has been deleted
-        self.deleted_content = deleted_content      # ???
+        self.deleted_content = deleted_content      # If true, everything has been deleted
         self.no_comments = no_comments              # Number of comments
         self.comments = comments                    # List of comments
         self.comments_next = comments_next          # ID for next stack of comments
@@ -76,61 +79,115 @@ class Post:
 class Posts:
     def __init__(self, posts: List[Post], next: str, **kwargs):
         self.posts = posts                          # List of post objects
-        self.next = next                            # ID for next set of comments
+        self.next = next                            # ID for next set of posts
         self.__dict__.update(kwargs)
 
-'''
-Post data model. 
+class Image:
+    def __init__(self, mimetype: str, width: int, height: int, size: int, average_color: str, 
+                 url: str, **kwargs):
+        self.mimetype = mimetype                    # seems to be image/jpeg for most
+        self.width = width                          # width of image
+        self.height = height                        # height of image
+        self.size = size                            # overall image size
+        self.average_color = average_color          # average colour in the image
+        self.url = url                              # url of stored image
+        self.__dict__.update(kwargs)
 
-https://discuit.net/api/posts?communityId=17692e122def73f25bd757e0
+class CommunityRule:
+    def __init__(self, id: int, rule: str, description: Optional[str], community_id: str, z_index: int, 
+                 created_by: str, created_at: datetime, **kwargs):
+        self.id = id                                # ID of rule
+        self.rule = rule                            # The actual rule
+        self.description = description              # Could be null
+        self.community_id = community_id            # ID of community its a rule in
+        self.z_index = z_index                      # Determines rule ordering
+        self.created_by = created_by                # User who created the rule
+        self.created_at = created_at                # When it was created
+        self.__dict__.update(kwargs)
 
-{
-    'posts' : [                                             list
-        {
-            "id": "177b8f0c5378c9a2fb17f509",               str
-            "type": "link",                                 str
-            "publicId": "bSfp10ml",                         str
-            "userId": "177a18d800357d19d2d73b91",           str
-            "username": "Oggidahh",                         str
-            "userGroup": "normal",                          str
-            "userDeleted": false,                           bool
-            "isPinned": false,                              bool
-            "communityId": "17692e122def73f25bd757e0",      str
-            "communityName": "general",                     str
-            "communityProPic: {}                            dict
-            "communityBannerImage : {}                      dict
-            "title": "Call It Whatevahh",                   str
-            "body": null,                                   str
-            "link" :                                        dict
-            {                                      
-                "link" : "hhtps..."                         str
-                "hostname" : "youtube.com"                  str
-                "image" : {}                                dict
-            }            
-            "locked": false,                                bool
-            "lockedBy": null,                               str
-            "lockedAt": null,                               str
-            "upvotes": 4,                                   int
-            "downvotes": 0,                                 int
-            "hotness": 376032970667,                        int
-            "createdAt": "2023-08-15T12:42:48Z",            str
-            "editedAt": null,                               str
-            "lastActivityAt": "2023-08-15T12:42:48Z",       str
-            "deleted": false,                               bool
-            "deletedAt": null,                              str
-            "deletedContent": false,                        bool
-            "noComments": 0,                                int
-            "comments": null,                               
-            "commentsNext": null,                           
-            "userVoted": null,
-            "userVotedUp": null                         
-        },
-        {
-            ---- NEXT POST ----
-        }
-    ],
-    "next": "177b57b436c86781b3cc5710"                      str
+class User:
+    def __init__(self, id: str, username: str, email: None, email_confirmed_at: None, 
+                 about_me: str, points: int, is_admin: bool, no_posts: int, no_comments: int, 
+                 created_at: datetime, deleted_at: None, banned_at: None, is_banned: bool, 
+                 notifications_new_count: int, modding_list: List, **kwargs):
+        self.id = id                                            # User ID
+        self.username = username                                # Username
+        self.email = email                                      # Can be null
+        self.email_confirmed_at = email_confirmed_at            # Can be null
+        self.about_me = about_me                                # About me description
+        self.points = points                                    # Points?
+        self.is_admin = is_admin                                # If the User is a site admin
+        self.no_posts = no_posts                                # Number of posts made by user
+        self.no_comments = no_comments                          # Number of comments made by user
+        self.created_at = created_at                            # Datetime when the account was created
+        self.deleted_at = deleted_at                            # Datetime when it was deleted
+        self.banned_at = banned_at                              # Datetime when it was banned
+        self.is_banned = is_banned                              # If the user is banned
+        self.notifications_new_count = notifications_new_count  # Number of new notifications user has
+        self.modding_list = modding_list                        # List of communities the user mods
+        self.__dict__.update(**kwargs)
 
-}
 
-'''     
+class Community:
+    def __init__(self, id: str, user_id: str, name: str, nsfw: bool, about: str, no_members: int, 
+                 pro_pic: Image, banner_image: Image, created_at: datetime, deleted_at: Optional[datetime], 
+                 user_joined: Optional[bool], user_mod: Optional[bool], mods: Optional[List[User]], 
+                 rules: Optional[List[CommunityRule]], reports_details: List[Dict], **kwargs):
+        self.id = id                                # Community ID
+        self.user_id = user_id                      # ID of user who created community
+        self.name = name                            # Community name (i.e., formula1, chess)
+        self.nsfw = nsfw                            # If community is NSFW
+        self.about = about                          # Description of communiity
+        self.no_members = no_members                # How many users have joined
+        self.pro_pic = pro_pic                      # Image object of the photo
+        self.banner_image = banner_image            # Image object
+        self.created_at = created_at                # Datetime the community was created
+        self.deleted_at = deleted_at                # Datetime it was deleted
+        self.user_joined = user_joined              # If the current AUTH user is subbed
+        self.user_mod = user_mod                    # If the current auth user is a mod
+        self.mods = mods                            # List of users who are mods
+        self.rules = rules                          # List of CommunityRules
+        self.reports_details = reports_details      # List of reports to the community
+        self.__dict__.update(kwargs)
+
+class Comment:
+    def __init__(self, id: str, post_id: str, post_public_id: str, community_id: str, 
+                 community_name: str, user_id: str, username: str, user_group: str, 
+                 user_deleted: bool, parent_id: Optional[str], depth: int, no_replies: int, 
+                 no_replies_direct: int, ancestors: Optional[List[str]], body: str, 
+                 upvotes: int, downvotes: int, created_at: datetime, edited_at: Optional[datetime], 
+                 deleted_at: Optional[datetime], user_voted: Optional[bool], user_voted_up: Optional[bool], 
+                 post_deleted: bool, **kwargs):
+        self.id = id                                # ID of comment
+        self.post_id = post_id                      # Post ID the comment is on
+        self.post_public_id = post_public_id        # Public post ID (discuit.com/postID)
+        self.community_id = community_id            # Community ID comment is in
+        self.community_name = community_name        # Community Name
+        self.user_id = user_id                      # User ID who made the comment
+        self.username = username                    # USername who made the comment
+        self.user_group = user_group                # Admin, mod, normal
+        self.user_deleted = user_deleted            # If author accoutn is deleted
+        self.parent_id = parent_id                  # Parent comment ID, can be null
+        self.depth = depth                          # Top-most comments have depth of 0
+        self.no_replies = no_replies                # Total number of replies to the comment
+        self.no_replies_direct = no_replies_direct  # Number of direct replies
+        self.ancestors = ancestors                  # List of comment IDs, starting at topmost
+        self.body = body                            # Comment body
+        self.upvotes = upvotes                      # Number of up
+        self.downvotes = downvotes                  # Number of down
+        self.created_at = created_at                # Datetime it was created
+        self.edited_at = edited_at                  # Datetime edited, can be null
+        self.deleted_at = deleted_at                # Datetime deleted, can be null
+        self.user_voted = user_voted                # If currently auth'd user voted 
+        self.user_voted_up = user_voted_up          # If auth user voteed up
+        self.post_deleted = post_deleted            # If the post the comment is on is delted
+        self.__dict__.update(kwargs)
+
+
+class Comments:
+    comments: List[Comment]
+    next: None
+
+    def __init__(self, comments: List[Comment], next: None) -> None:
+        self.comments = comments
+        self.next = next
